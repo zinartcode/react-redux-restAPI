34d1cae15acc8003b55c9b35c55d595d1cda47d3
Arthur <zinartinc@gmail.com>
2018-09-17 21:22:35 -0400

    compnents routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import logo1 from './logo1.svg';
+import logo2 from './logo2.svg';
 import './App.css';
 import Posts from './components/posts';
+import PostForm from './components/postForm';
 
 class App extends Component {
   render() {
@@ -11,7 +14,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+        </header> */}
+        <PostForm />
+        <hr/>
         <Posts />
       </div>
     );
---
+++ src/components/postForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class PostForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <form action=""></form>
+      </div>
+    )
+  }
+}
+
+export default PostForm;
