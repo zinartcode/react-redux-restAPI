234c11dc13144d2d7da3a2e827735c10508a0d79
Arthur <zinartinc@gmail.com>
2018-09-19 23:04:10 -0400

    submit fetch data done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Posts from './components/posts';
 import PostForm from './components/postForm';
